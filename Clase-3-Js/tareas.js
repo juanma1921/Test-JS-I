772b13fe33bc54ccbf1b4bcedd1e7e98234a33b7
juan manuel lemos frich <vanapares@gmail.com>
2022-09-04 17:46:25 -0300

    primera

--- Clase-3-Js/tareas.js
+++ Clase-3-Js/tareas.js
@@ -5,12 +5,18 @@ function obtenerMayor(x, y) {
   // Devuelve el número más grande
   // Si son iguales, devuelve cualquiera de los dos
   // Tu código:
+  return Math.max(x, y);
 }
 
 function mayoriaDeEdad(edad) {
   //Determinar si la persona según su edad puede ingresar a un evento.
   //Si tiene 18 años ó más, devolver --> "Allowed"
   //Si es menor, devolver --> "Not allowed"
+  if (edad>=18) {
+    return "Allowed";
+  } else {
+    return "Not allowed";
+  }
 }
 
 function conection(status) {
@@ -19,6 +25,13 @@ function conection(status) {
   //Cuando el estado es igual a 2, el usuario está "Away"
   //De lo contrario, presumimos que el usuario está "Offline"
   //Devolver el estado de conexión de usuario en cada uno de los casos.
+  if (status===1) {
+    return "Online";
+  } else if (status===2){
+    return "Away";
+  } else {
+    return "Offline";
+  }
 }
 
 function saludo(idioma) {
@@ -28,6 +41,15 @@ function saludo(idioma) {
   // Si "idioma" es "ingles", devuelve "Hello!"
   // Si "idioma" no es ninguno de los anteiores o es `undefined` devuelve "Hola!"
   // Tu código:
+  if (idioma==="aleman") {
+    return "Guten Tag!";
+  } else if (idioma==="mandarin"){
+    return "Ni Hao!";
+  } else if (idioma==="ingles"){
+    return "Hello!";
+  } else {
+    return "Hola!";
+  }
 }
 
 function colors(color) {
@@ -38,18 +60,52 @@ function colors(color) {
   //En caso que el color recibido sea "orange", devuleve --> "This is orange"
   //Caso default: devuelve --> "Color not found"
   //Pista: Puedes usar el statement Switch.
+  switch (color) {
+    case "blue":
+      return "This is blue";
+      break;
+    case "red":
+      return "This is red";
+      break;
+    case "green":
+      return "This is green";
+      break;
+    case "orange":
+      return "This is orange";
+      break;
+    default:
+      return "Color not found";
+      break;
+  }
 }
 
 function esDiezOCinco(numero) {
   // Devuelve "true" si "numero" es 10 o 5
   // De lo contrario, devuelve "false"
   // Tu código:
+  switch (numero) {
+    case 10:
+      return true;
+      break;
+    case 5:
+      return true;
+      break;
+    default:
+      return false
+      break;
+  }
 }
 
 function estaEnRango(numero) {
   // Devuelve "true" si "numero" es menor que 50 y mayor que 20
   // De lo contrario, devuelve "false"
   // Tu código:
+  switch (numero > 20 && numero < 50) {
+    case true:
+      return true;
+    default:
+      return false;
+  }
 }
 
 function esEntero(numero) {
@@ -60,6 +116,14 @@ function esEntero(numero) {
   // De lo contrario, devuelve "false"
   // Pista: Puedes resolver esto usando `Math.floor`
   // Tu código:
+  switch (numero - Math.floor(numero)===0) {
+    case true:
+      return true;
+      break;
+    default:
+      return false;
+      break;
+  }
 }
 
 function fizzBuzz(numero) {
@@ -67,6 +131,20 @@ function fizzBuzz(numero) {
   // Si "numero" es divisible entre 5, devuelve "buzz"
   // Si "numero" es divisible entre 3 y 5 (ambos), devuelve "fizzbuzz"
   // De lo contrario, devuelve el numero
+  if (numero % 3 === 0 && numero % 5 === 0) {
+    return "fizzbuzz";
+}
+
+if (numero % 3 === 0) {
+    return "fizz";
+}
+
+if (numero % 5 === 0) {
+    return "buzz";
+}
+
+return numero;
+
 }
 
 function operadoresLogicos(num1, num2, num3) {
@@ -77,6 +155,18 @@ function operadoresLogicos(num1, num2, num3) {
   //0 no es ni positivo ni negativo. Si alguno de los argumentos es 0, retornar "Error".
   //Si no se cumplen ninguna de las condiciones anteriores, retornar false. 
   //Tu código:
+  if (num1 === 0 || num2 === 0 || num3 === 0 ) {
+    return "Error";
+  } else if (num1 > num2 && num1 > num3 && num1 > 0) {
+    return "Número 1 es mayor y positivo";
+  } else if (num1 < 0 || num2 < 0 || num3 < 0 ) {
+    return "Hay negativos";
+  } else if (num3 > num1 && num3 > num2) {
+    num3++;
+    return num3;
+  } else {
+    return false;
+  }
 }
 
 function esVerdadero(valor) {
